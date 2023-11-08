fdb24b95313507032112dd69141f4584f97734b1
Mohammed <elamrani.mohammed95@gmail.com>
2023-11-08 23:22:06 +0100

    user ajoute avec mdp hashe

--- model/user.js
+++ model/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-const crypto = require("crypto")
-const jwt = require("jsonwebtoken")
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
 const userSchema = new Schema (
     {
 
